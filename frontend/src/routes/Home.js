20ab5c7d9173395e1ed8bfe32e434e15955a036b
ryu <rjsduf0503@naver.com>
2021-07-20 17:19:20 +0900

    <fix>(<home>: fixed home.js with session check by using Django session)

--- frontend/src/routes/Home.js
+++ frontend/src/routes/Home.js
@@ -1,12 +1,26 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import Session from '../components/SessionCheck';
 
-export default () => (
-    <>
+export default () => {
+    const noSession = () => {
+        alert("로그인이 필요합니다.");
+    }
+    return(
         <div className="contents1">
-            <Link to='/pushup/guideLine'><button className="lightest bigBtn">푸시업</button></Link>
-            <Link to='/squat/guideLine'><button className="neutral bigBtn">스쿼트</button></Link>
-            <Link to='./statistics'><button className="darkest bigBtn">통계</button></Link>
+            {<Session/> ? 
+                    <>
+                        <Link to='/pushup/guideLine'><button className="lightest bigBtn">푸시업</button></Link>
+                        <Link to='/squat/guideLine'><button className="neutral bigBtn">스쿼트</button></Link>
+                        <Link to='/statistics'><button className="darkest bigBtn">통계</button></Link>
+                    </>
+                :
+                    <>
+                        <button className="lightest bigBtn" onClick={noSession}>푸시업</button>
+                        <button className="neutral bigBtn" onClick={noSession}>스쿼트</button>
+                        <button className="darkest bigBtn" onClick={noSession}>통계</button>
+                    </>
+            }
         </div>
-    </>
-);
+    )
+};
