fad4a16964b33fb41c1ed0beaa628204e09e773a
Si-jeong <ssonge413@gmail.com>
2021-07-23 01:45:31 +0900

    refactor: rearrange code

--- backend/server.py
+++ backend/server.py
@@ -296,7 +296,7 @@ def analyze_squat():
     return jsonify({'count': squat_count, "guide": squat_guide})       
 
 
-######################## 로그인 ########################
+######################## 사용자 api ########################
 
 def encode_auth_token(user_id):
     """
@@ -523,6 +523,63 @@ def checkAuth():
 def logout():
     return User().logout()
 
+####################### 운동 api ########################
+
+class Exercise:
+
+    def updateExercise(self):
+        print(request.is_json)
+        exercise = request.get_json()
+
+        # exercise = {
+        #     "id": 
+        #     "exerDate": "2020-07-14"
+        #     "exerType": "1" or "2"
+        #     "exerNum": 정수
+        #     "exerTime": 정수 (초 단위)  
+        # }
+        
+        # db.exercises에 id, exerDate가 일치하는 운동 기록 검색
+        # 만약 일치하는 운동 기록 이미 있다면 squarNum/pushupNum, everTime을 preNum, preTime변수에 저장
+        # db의 squarNum/pushNum, exerTime 갱신
+
+        prePN, preSN, preTime = 0, 0, 0
+        if db.exercises.find_one({"id": exercise['id'], "exerDate": exercise['exerDate']}):
+            pre = db.exercises.find_one({"id": exercise['id'], "exerDate": exercise['exerDate']})
+            prePN = pre['pushupNum']
+            preSN = pre['squartNum']
+            preTime = pre['exerTime']
+        if exercise['exerType'] == "1":
+            db.exercises.update_one(
+                {"id": exercise['id'], "exerDate": exercise['exerDate'] },
+                {"$set": {"squartNum": preSN + exercise['exerNum'], "pushupNum": prePN, "exerTime": preTime + exercise['exerTime']}}, 
+                upsert = True
+            )
+        else:
+            db.exercises.update_one(
+                {"id": exercise['id'], "exerDate": exercise['exerDate'] },
+                {"$set": {"squartNum": preSN, "pushupNum": prePN + exercise['exerNum'], "exerTime": preTime + exercise['exerTime']}}, 
+                upsert = True
+            )
+
+        return jsonify(exercise), 200
+
+    def showExercises(self):
+        user_info = request.get_json()
+        
+        exercises = db.exercise.find_one({"id": user_info['id'], "exerDate": user_info['exerDate']})
+        if exercises:
+            return jsonify(exercises), 200
+        return jsonify({ "error": "No workout records" }), 402
+
+# db.exercises.document = {
+#     "id": "~~",
+#     "exerDate": "2021-07-14",
+#     "squarNum": 정수,
+#     "pushupNum": 정수,
+#     "everTime": 정수 (초 단위)
+# }
+
 @app.route('/recordex', methods=['POST'])
 def updateExercise():
     return Exercise().updateExercise()
@@ -531,6 +588,8 @@ def updateExercise():
 def showExercises():
     return Exercise().showExercises()
 
+
+####################### rest api ########################
 @ns.route('/<string:text>')
 @ns.response(200, 'Found')
 @ns.response(404, 'Not found')
