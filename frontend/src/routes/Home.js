f71bc946248a0a7175167e25f2f7081e644e0e20
ryu <rjsduf0503@naver.com>
2021-07-23 16:41:18 +0900

    fix: ternary operator's order

--- frontend/src/routes/Home.js
+++ frontend/src/routes/Home.js
@@ -19,16 +19,16 @@ export default () => {
         <div className="contents1">
             {auth ? 
                     <>
-                        <button className="lightest bigBtn boxShadow" onClick={noSession}>푸시업</button>
-                        <button className="neutral bigBtn boxShadow" onClick={noSession}>스쿼트</button>
-                        <button className="darkest bigBtn boxShadow" onClick={noSession}>통계</button>
-                    </>
-                :
-                    <>
                         <Link to='/pushup/guideLine'><button className="lightest bigBtn boxShadow">푸시업</button></Link>
                         <Link to='/squat/guideLine'><button className="neutral bigBtn boxShadow">스쿼트</button></Link>
                         <Link to='/statistics'><button className="darkest bigBtn boxShadow">통계</button></Link>
                     </>
+                :
+                <>
+                    <button className="lightest bigBtn boxShadow" onClick={noSession}>푸시업</button>
+                    <button className="neutral bigBtn boxShadow" onClick={noSession}>스쿼트</button>
+                    <button className="darkest bigBtn boxShadow" onClick={noSession}>통계</button>
+                </>
             }
         </div>
     )
