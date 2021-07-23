7448487f0c492c64bbeeec27d78cca71aae8a6c1
ryu <rjsduf0503@naver.com>
2021-07-23 18:07:30 +0900

    feat: added header css with SVG background

--- frontend/src/components/GlobalStyles.js
+++ frontend/src/components/GlobalStyles.js
@@ -21,6 +21,38 @@ const GlobalStyles = createGlobalStyle`
         font-size: inherit;
         text-decoration: none;
     }
+
+    .textShadow{
+        text-shadow: 3px 3px 5px #606060;
+    }
+
+    .hontLogo{
+        -webkit-filter: drop-shadow(3px 3px 3px gray) invert(100%) sepia(0%) saturate(2%) hue-rotate(241deg) brightness(103%) contrast(101%);
+        filter: drop-shadow(4px 4px 2px #8F8F8F) invert(100%) sepia(0%) saturate(2%) hue-rotate(241deg) brightness(103%) contrast(101%);
+        height: 90px;
+        width: 100px;
+    }
+
+    .headerContainer{
+        width: 100%;
+        display: flex;
+        flex-flow: row wrap;
+        justify-content: space-between;
+        align-items: center;
+    }
+
+    .headerItem{
+        flex-grow: 0;
+    }
+
+    .headerItem:nth-child(1){
+        flex-shrink: 0;
+        flex-basis: 600px;
+    }
+
+    .headerItem:nth-child(2){
+        flex-grow: 1;
+    }
 `;
 
 export default GlobalStyles;
--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,19 +1,54 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import hontLogo from '../images/Logo.png';
+import React, { useState, useEffect } from 'react';
+import { Link, withRouter } from 'react-router-dom';
+import styled from "styled-components";
+import { SVG1, SVG2 } from './SVG';
+import hontLogo from '../image/Logo.png';
 import axios from "axios";
 
-export default () => {
+const Logo = styled.span`
+    color: #FFFFFF;
+    font-size: 130px;
+    margin-top: 20px;
+    margin-left: 100px;
+    display: flex;
+`;
+
+const Header = styled.header`
+  font-family: "Baloo Bhaijaan", sans-serif;
+  position: relative;
+  width: 100%;
+  font-weight: 600;
+`;
+
+const Nav = styled.span`
+  color: #FFFFFF;
+  font-size: 60px;
+  margin-right: 100px;
+`;
+
+const Margin = styled.span`
+  margin-right: 50px;
+  margin-left: 50px;
+`;
+
+export default withRouter(({ location: { pathname }}) => {
+  const isSVG2 =  pathname === "/" || 
+                  pathname === "/login" ||
+                  pathname === "/signup" || 
+                  pathname === "/Result" ||
+                  pathname === "/statistics";
+
   const [auth, setAuth] = useState("");
 
   useEffect(() => {
+    console.log(localStorage.getItem('token'));
     if (localStorage.getItem('token') !== null){
       setAuth(true);
     }
     else {
       setAuth(false);
     }
-  }, [])
+  }, localStorage.getItem('token'))
 
   const handleLogout = async () => {
     const postUrl = "http://localhost:5000/auth/logout";
@@ -36,21 +71,29 @@ export default () => {
       }
     })
   }
-
-  return (
-    <header>
-      <span><Link to='/'>
-                <img src={hontLogo}/>
-                <span>HONT</span>
-            </Link>
-      </span>
-      <Link to='/signup'>Signup</Link>
-      {auth ? 
+  return(
+    <Header className="headerContainer">
+          {isSVG2 ? <SVG2/> : <SVG1/>}
+          <div className="headerItem">
+            <Logo>
+              <Link to='/'>
+                <img src={hontLogo} className="hontLogo"/>
+                <span className="textShadow">HONT</span>
+              </Link>
+            </Logo>
+          </div>
+          <div className="headerItem">
+            <Nav className="textShadow">
+              {auth ? 
                   <Link to='/' onClick={handleLogout}>Logout</Link>
                 : 
-                  <><Link to='/signup'>Signup</Link>
+                  <><Margin>
+                    <Link to='/signup'>Signup</Link>
+                  </Margin>
                   <Link to='/login'>Login</Link></>
               } 
-    </header>
-  )
-}
+            </Nav>
+          </div>
+    </Header>
+  );
+});
