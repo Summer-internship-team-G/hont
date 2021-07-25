daaa1038549bf1a4f3d33cf56fa2bac58e5aa001
ryu <rjsduf0503@naver.com>
2021-07-25 17:02:35 +0900

    style: added Footer CSS

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -1,9 +1,28 @@
+import styled from "styled-components";
+
+const Footer = styled.footer`
+    padding-top: 5px;
+    z-index: 20;
+    width: 100%;
+    height: 80px;
+    text-align: center;
+    position: relative;
+    bottom: 0;
+    padding-top: 20px;
+`;
+
+const Div = styled.div`
+    color: black;
+    font-weight: 400;
+`;
+
+
 export default () => {
     return (
-        <footer>
-            <div>권예진 류건열 김시정 장민지 최예은</div>
-            <div>SilliconValley Online Internship</div>
-            <div>ZoomMate Team. All rights reserved.</div>
-        </footer>
+        <Footer>
+            <Div>권예진 류건열 김시정 장민지 최예은</Div>
+            <Div>SilliconValley Online Internship</Div>
+            <Div>ZoomMate Team. All rights reserved.</Div>
+        </Footer>
     )
 }
