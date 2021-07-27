69415ef2eb2bbd59de10810042f440226b8f6292
Si-jeong <ssonge413@gmail.com>
2021-07-28 04:22:56 +0900

    refactor: post to get method

--- backend/server.py
+++ backend/server.py
@@ -113,17 +113,18 @@ def connectToPolly():
     )
     return polly_client
 
-@app.route('/textToSpeech', methods=['POST'])
-def textToSpeech():
-    data = request.get_json()
-    text = data.get('count', '')
+@app.route('/textToSpeech/<int:count>', methods=['GET'])
+def textToSpeech(count):
+    print("=== textToSpeech start ====")
+
     polly_client = connectToPolly()
     response = polly_client.synthesize_speech(
-        Text="<speak>" + text + "</speak>",    
+        Text="<speak>" + str(count) + "</speak>",    
         VoiceId='Seoyeon',
         OutputFormat='mp3',
         TextType='ssml'
         )
+
     return send_file(response.get("AudioStream"), mimetype="audio/mp3")
 
 ######################## swagger ########################
