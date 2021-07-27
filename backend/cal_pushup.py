dbca49fc30889eca47960623012abb3496af983a
Si-jeong <ssonge413@gmail.com>
2021-07-27 13:27:15 +0900

    chore: Update cal_pushup.py

--- backend/cal_pushup.py
+++ backend/cal_pushup.py
@@ -8,7 +8,7 @@ def get_angle_v3(p1, p2, p3):
 ##푸시업 한 기준
 
 def is_pushup(elbow_angle, body_angle):
-	pushup_count = 0
+	print("======is_pushup start=======")
 	pushup_guide= ""
 	action_status = True
 
@@ -16,11 +16,10 @@ def is_pushup(elbow_angle, body_angle):
 		action_status = False
 
 	else: 
-		if action_status == False :
-			pushup_count+= 1
+		if action_status == False:
 			action_status = True
 			
-		if elbow_angle < 70 :
+		if elbow_angle < 70:
 			pushup_guide +='너무 내려가셨어요! 팔꿈치 각도를 90도로 맞춰주세요\n'
 		if elbow_angle > 100:           
 			pushup_guide += '조금 더 내려가주세요! 팔꿈치 각도를 90도로 맞춰주세요\n'
@@ -28,8 +27,14 @@ def is_pushup(elbow_angle, body_angle):
 			pushup_guide += '엉덩이를 내려서 몸이 일직선이 되도록 맞춰주세요\n'
 		if body_angle > 200:
 			pushup_guide += '엉덩이를 올려서 몸이 일직선이 되도록 맞춰주세요\n'
+
+	if not action_status:
+		text = "성공"
+	else:
+		text = "실패"
+	print("======is_pushup end=====")
 	
-	return pushup_count, pushup_guide
+	return action_status, pushup_guide, text
 
 #푸시업 - 올바른 푸시업 자세 기준을 만든걸 던져줌
 def do_pushup(l_sh, r_sh, l_elbow, r_elbow, l_wrist, r_wrist, l_hip, r_hip, l_ankle, r_ankle):
@@ -41,7 +46,7 @@ def do_pushup(l_sh, r_sh, l_elbow, r_elbow, l_wrist, r_wrist, l_hip, r_hip, l_an
 	elbow_angle = (l_elbow_angle + r_elbow_angle) / 2
 	body_angle = (l_body_angle + r_body_angle) / 2
 
-	p_count, p_guide = is_pushup(elbow_angle, body_angle)
+	p_count, p_guide, text = is_pushup(elbow_angle, body_angle)
 
-	return p_count, p_guide
+	return p_count, p_guide, text
 
