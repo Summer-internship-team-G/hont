922f0ca00e6e42aa9aa95ebe6278c869760b225a
Si-jeong <ssonge413@gmail.com>
2021-07-28 22:56:46 +0900

    feat: Make count in Korean

--- backend/server.py
+++ backend/server.py
@@ -104,28 +104,52 @@ def logout():
 
 ####################### TTS api ########################
 def connectToPolly():
-    region_name = ''
+    region_name = 'ap-northeast-2'
     polly_client = boto3.client(
         'polly',
-        aws_access_key_id = '',
-        aws_secret_access_key = '',
+        aws_access_key_id = 'AKIAU6ECZPVOP6A4VROO',
+        aws_secret_access_key = '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',
         region_name = region_name
     )
     return polly_client
 
-@app.route('/textToSpeech/<int:count>', methods=['GET'])
-def textToSpeech(count):
-    print("=== textToSpeech start ====")
-
-    polly_client = connectToPolly()
-    response = polly_client.synthesize_speech(
-        Text="<speak>" + str(count) + "</speak>",    
-        VoiceId='Seoyeon',
-        OutputFormat='mp3',
-        TextType='ssml'
+class TTS:
+    precount = 0
+    def textToSpeech(self, count):
+        num = (count-1)%10 +1
+        korean = {
+            "0": " ",
+            "1": "하나",
+            "2": "둘",
+            "3": "셋",
+            "4": "넷",
+            "5": "다섯", 
+            "6": "여섯",
+            "7": "일곱",
+            "8": "여덟",
+            "9": "아홉",
+            "10": "열"
+        }
+        text = " "
+        if TTS.precount != count:
+            text = korean[str(num)]
+            TTS.precount = count
+        print("TTS.precount: ", TTS.precount)
+        print("count: ", count, "num: ", num)
+        print("text: ", text)
+        polly_client = connectToPolly()
+        response = polly_client.synthesize_speech(
+            Text="<speak>" + text + "</speak>",
+            VoiceId='Seoyeon',
+            OutputFormat='mp3',
+            TextType='ssml'
         )
+        return send_file(response.get("AudioStream"), mimetype="audio/mp3", cache_timeout=0)
 
-    return send_file(response.get("AudioStream"), mimetype="audio/mp3")
+@app.route('/textToSpeech/<int:count>', methods=['GET'])
+@cross_origin()
+def textToSpeech(count):
+    return TTS().textToSpeech(count).headers.add("Access-Control-Allow-Origin", "*")
 
 ######################## swagger ########################
 @ns.route('/<string:text>')
